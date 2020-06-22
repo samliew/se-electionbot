47950e0c92309f0d32eb27e5a9f86d4dc9679cc7
Samuel Liew <samliew@gmail.com>
2020-06-22 13:42:05 +0800

    Add messages, update faq list

--- src/index.js
+++ src/index.js
@@ -306,9 +306,10 @@ const main = async () => {
                 responseText = `I'm ${me.name} and ${me.about}`;
             }
             else if(['help', 'commands', 'faq', 'info', 'list'].some(x => content.includes(x))) {
-                responseText = '\n' + ['FAQ topics I can help with:', 
-                    'how does the election work', 'who are the candidates', 'how to nominate', 'how to vote', 'how to decide who to vote for', 
-                    'how many voted', 'who are the current moderators', 'election status', 'election schedule', 
+                responseText = '\n' + ['Examples of FAQ topics I can help with:', 
+                    'how does the election work', 'who are the candidates', 'how to nominate', 'how to vote', 
+                    'how to decide who to vote for', 'how many voted', 'why be a moderator', 'what are the moderator benefits',
+                    'are moderators paid', 'who are the current moderators', 'election status', 'election schedule', 
                     'how is candidate score calculated', 'moderation badges', 'participation badges', 'editing badges',
                 ].join('\n- ');
             }
@@ -393,9 +394,15 @@ const main = async () => {
             }
 
             // Why be a moderator
-            else if(['why', 'what'].some(x => content.includes(x)) && [' be ', 'benefit', 'pros', 'should', 'entail', 'reward', 'compensat'].some(x => content.includes(x)) && ['mod'].every(x => content.includes(x))) {
+            else if(['why', 'what', 'are', 'is', 'should'].some(x => content.includes(x)) && ['be a', 'become', 'benefit', 'pros', 'entail'].some(x => content.includes(x)) && ['mod'].every(x => content.includes(x))) {
                 responseText = `[Elected ♦ moderators](${election.siteurl}/help/site-moderators) are essential to keeping the site clean, fair, and friendly. ` + 
                   `Not only that, moderators get [additional privileges](https://meta.stackexchange.com/q/75189) like viewing deleted posts/comments/chat messages, searching for a user's deleted posts, suspend/privately message users, migrate questions to any network site, unlimited binding close/delete/flags on everything, just to name a few.`;
+            }
+
+            // Are moderators paid
+            else if(['why', 'what', 'are', 'how'].some(x => content.includes(x)) && ['reward', 'paid', 'compensat', 'money'].some(x => content.includes(x)) && ['mod'].every(x => content.includes(x))) {
+                responseText = `[Elected ♦ moderators](${election.siteurl}/help/site-moderators) are essential to keeping the site clean, fair, and friendly. ` + 
+                  `This is a voluntary role, and moderators do not get paid by Stack Exchange.`;
             }
 
             // What is election
