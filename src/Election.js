b224003c693dc7f079d6f41c378db750f27d2435
Oleg Valter <o.a.philippov@gmail.com>
2021-06-06 19:35:31 +0300

    removed unused import, linted, type fixed

--- src/Election.js
+++ src/Election.js
@@ -1,4 +1,3 @@
-const request = require('request-promise');
 const cheerio = require('cheerio');
 const utils = require('./utils');
 
@@ -7,7 +6,7 @@ export default class Election {
     constructor(electionUrl, electionNum = null) {
         this.electionUrl = electionUrl;
 
-        if(electionNum) {
+        if (electionNum) {
             this.electionNum = Number(electionNum);
         }
         else {
@@ -41,27 +40,27 @@ export default class Election {
         // Save prev values so we can compare changes after
         this._prevObj = Object.assign({}, this); // fast way of cloning an object
         this._prevObj._prevObj = null;
-        
+
         const electionPageUrl = `${this.electionUrl}?tab=nomination`;
 
         try {
             const pageHtml = await utils.fetchUrl(electionPageUrl);
-            
+
             // Parse election page
-            const $ = cheerio.load(pageHtml);
+            const $ = cheerio.load(/** @type {string} */(pageHtml));
 
             let electionPost = $('#mainbar .post-text .wiki-ph-content');
             let sidebarValues = $('#sidebar').find('.label-value').map((i, el) => $(el).attr('title') || $(el).text()).get();
 
             // Insert null value in second position for elections with no primary phase
-            if(sidebarValues.length == 5) {
-                sidebarValues.splice(1, 0, null); 
+            if (sidebarValues.length == 5) {
+                sidebarValues.splice(1, 0, null);
             }
 
             this.updated = Date.now();
             this.sitename = $('meta[property="og:site_name"]').attr('content').replace('Stack Exchange', '').trim();
             this.siteHostname = this.electionUrl.split('/')[2]; // hostname only, exclude trailing slash
-            this.siteUrl = 'https://' + this.siteHostname; 
+            this.siteUrl = 'https://' + this.siteHostname;
             this.title = $('#content h1').first().text().trim();
             this.dateNomination = sidebarValues[0];
             this.datePrimary = sidebarValues[1];
@@ -72,43 +71,42 @@ export default class Election {
             this.repVote = 150;
             this.repNominate = Number($('#sidebar .s-sidebarwidget--content b').eq(1).text().replace(/\D+/g, ''));
             this.arrWinners = [];
-            this.arrNominees = $('#mainbar .candidate-row').map((i, el) => 
-                {
-                    return {
-                        userId: Number($(el).find('.user-details a').attr('href').split('/')[2]),
-                        userName: $(el).find('.user-details a').text(),
-                        userYears: $(el).find('.user-details').contents().map(function() {
-                            if(this.type === 'text') return this.data.trim();
-                        }).get().join(' ').trim(),
-                        userScore: $(el).find('.candidate-score-breakdown').find('b').text().match(/(\d+)\/\d+$/)[0],
-                        permalink: electionPageUrl + '#' + $(el).attr('id'),
-                    }
-                }).get();
+            this.arrNominees = $('#mainbar .candidate-row').map((i, el) => {
+                return {
+                    userId: Number($(el).find('.user-details a').attr('href').split('/')[2]),
+                    userName: $(el).find('.user-details a').text(),
+                    userYears: $(el).find('.user-details').contents().map(function () {
+                        if (this.type === 'text') return this.data.trim();
+                    }).get().join(' ').trim(),
+                    userScore: $(el).find('.candidate-score-breakdown').find('b').text().match(/(\d+)\/\d+$/)[0],
+                    permalink: electionPageUrl + '#' + $(el).attr('id'),
+                };
+            }).get();
 
             this.qnaUrl = process.env.ELECTION_QA_URL || electionPost.find('a').not('[href*="/tagged/"]').not('[href*="/chat"]').not('[href*="stackoverflow.blog"]').last().attr('href');
             this.chatUrl = process.env.ELECTION_CHATROOM_URL || electionPost.find('a[href*="/rooms/"]').attr('href');
 
             // Calculate phase of election
             const now = Date.now();
-            this.phase = new Date(this.dateEnded) <= now ? 'ended' :
-                new Date(this.dateElection) <= now ? 'election' : 
-                this.datePrimary && new Date(this.datePrimary) <= now ? 'primary' : 
-                new Date(this.dateNomination) <= now ? 'nomination' : 
-                null; // default
+            this.phase = new Date(this.dateEnded).valueOf() <= now ? 'ended' :
+                new Date(this.dateElection).valueOf() <= now ? 'election' :
+                    this.datePrimary && new Date(this.datePrimary).valueOf() <= now ? 'primary' :
+                        new Date(this.dateNomination).valueOf() <= now ? 'nomination' :
+                            null; // default
 
             // If election has ended (or cancelled)
-            if(this.phase === 'ended') {
+            if (this.phase === 'ended') {
 
                 // Get results URL
                 this.resultsUrl = $('#mainbar').find('.question-status h2').first().find('a').first().attr('href');
-                if(!this.resultsUrl.includes('opavote.com')) this.resultsUrl = ''; // incorrect/not available immediately
-                
+                if (!this.resultsUrl.includes('opavote.com')) this.resultsUrl = ''; // incorrect/not available immediately
+
                 let winnerElem = $('#mainbar').find('.question-status h2').eq(1);
 
                 // Check if election was cancelled?
-                if(winnerElem.text().includes('cancelled')) {
+                if (winnerElem.text().includes('cancelled')) {
                     this.phase = 'cancelled';
-                    
+
                     // Convert link to chat-friendly markup
                     this.cancelledText = winnerElem.html()
                         .replace(/<a href="/g, 'See [meta](')
@@ -117,10 +115,10 @@ export default class Election {
                 // Election ended
                 else {
                     // Get election stats
-                    this.statVoters = winnerElem.contents().map(function() {
-                        if(this.type === 'text') return this.data.trim();
+                    this.statVoters = winnerElem.contents().map(function () {
+                        if (this.type === 'text') return this.data.trim();
                     }).get().join(' ').trim();
-                    
+
                     // Get winners
                     let winners = winnerElem.find('a').map((i, el) => Number($(el).attr('href').split('/')[2])).get();
                     this.arrWinners = this.arrNominees.filter(v => winners.includes(v.userId));
@@ -133,7 +131,7 @@ export default class Election {
                 `-------- QNA ${this.qnaUrl};  CHAT ${this.chatUrl}`
             );
         }
-        catch(err) {
+        catch (err) {
             console.error(`SCRAPE - Failed scraping ${this.electionUrl}`, err);
         }
     }
