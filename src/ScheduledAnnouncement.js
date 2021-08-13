fcdd09ea46e8b5498c573d95b00498f05d4e5f66
Oleg Valter <o.a.philippov@gmail.com>
2021-08-13 14:49:59 +0300

    fixed dateToUtcTimestamp method name; linted

--- src/ScheduledAnnouncement.js
+++ src/ScheduledAnnouncement.js
@@ -6,13 +6,13 @@ export default class ScheduledAnnouncement {
     constructor(room, election) {
         this._room = room;
         this._election = election;
-    
+
         // Run the sub-functions once only
         this._nominationSchedule = null;
         this._primarySchedule = null;
         this._electionSchedule = null;
         this._winnerSchedule = null;
-        
+
         // Store task so we can stop if needed
         this._nominationTask = null;
         this._primaryTask = null;
@@ -30,7 +30,7 @@ export default class ScheduledAnnouncement {
             primary: this._primarySchedule,
             election: this._electionSchedule,
             ended: this._winnerSchedule
-        }
+        };
     }
 
     setRoom(room) {
@@ -40,12 +40,12 @@ export default class ScheduledAnnouncement {
     setElection(election) {
         this._election = election;
     }
-    
+
     initWinner(date) {
-        if(this._winnerSchedule != null || this._winnerTask != null) return false;
+        if (this._winnerSchedule != null || this._winnerTask != null) return false;
 
         const _endedDate = new Date(date);
-        if(_endedDate > Date.now()) {
+        if (_endedDate.valueOf() > Date.now()) {
             const cs = `0 ${_endedDate.getHours()} ${_endedDate.getDate()} ${_endedDate.getMonth() + 1} *`;
             this._winnerTask = cron.schedule(
                 cs,
@@ -61,10 +61,10 @@ export default class ScheduledAnnouncement {
     }
 
     initElection(date) {
-        if(this._electionSchedule != null || this._electionTask != null || typeof date == 'undefined') return false;
+        if (this._electionSchedule != null || this._electionTask != null || typeof date == 'undefined') return false;
 
         const _electionDate = new Date(date);
-        if(_electionDate > Date.now()) {
+        if (_electionDate.valueOf() > Date.now()) {
             const cs = `0 ${_electionDate.getHours()} ${_electionDate.getDate()} ${_electionDate.getMonth() + 1} *`;
             this._electionTask = cron.schedule(
                 cs,
@@ -80,10 +80,10 @@ export default class ScheduledAnnouncement {
     }
 
     initPrimary(date) {
-        if(this._primarySchedule != null || this._primaryTask != null || typeof date == 'undefined') return false;
+        if (this._primarySchedule != null || this._primaryTask != null || typeof date == 'undefined') return false;
 
         const _primaryDate = new Date(date);
-        if(_primaryDate > Date.now()) {
+        if (_primaryDate.valueOf() > Date.now()) {
             const cs = `0 ${_primaryDate.getHours()} ${_primaryDate.getDate()} ${_primaryDate.getMonth() + 1} *`;
             this._primaryTask = cron.schedule(
                 cs,
@@ -99,10 +99,10 @@ export default class ScheduledAnnouncement {
     }
 
     initNomination(date) {
-        if(this._nominationSchedule != null || this._nominationTask != null || typeof date == 'undefined') return false;
+        if (this._nominationSchedule != null || this._nominationTask != null || typeof date == 'undefined') return false;
 
         const _nominationDate = new Date(date);
-        if(_nominationDate > Date.now()) {
+        if (_nominationDate.valueOf() > Date.now()) {
             const cs = `0 ${_nominationDate.getHours()} ${_nominationDate.getDate()} ${_nominationDate.getMonth() + 1} *`;
             this._nominationTask = cron.schedule(
                 cs,
@@ -126,7 +126,7 @@ export default class ScheduledAnnouncement {
             async () => {
                 console.log('TEST CRON STARTED');
                 await this._election.scrapeElection();
-                await this._room.sendMessage(`Test cron job succesfully completed at ${utils.dateToTimestamp(this._election.updated)}.`);
+                await this._room.sendMessage(`Test cron job succesfully completed at ${utils.dateToUtcTimestamp(this._election.updated)}.`);
                 console.log('TEST CRON ENDED', this._election, '\n', this._room);
             },
             { timezone: "Etc/UTC" }
@@ -142,11 +142,11 @@ export default class ScheduledAnnouncement {
     }
 
     cancelAll() {
-        if(this._nominationTask != null) this._nominationTask.stop();
-        if(this._primaryTask != null) this._primaryTask.stop();
-        if(this._electionTask != null) this._electionTask.stop();
-        if(this._winnerTask != null) this._winnerTask.stop();
-        
+        if (this._nominationTask != null) this._nominationTask.stop();
+        if (this._primaryTask != null) this._primaryTask.stop();
+        if (this._electionTask != null) this._electionTask.stop();
+        if (this._winnerTask != null) this._winnerTask.stop();
+
         this._nominationSchedule = null;
         this._primarySchedule = null;
         this._electionSchedule = null;
