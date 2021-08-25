98cccd3ae88c58aa15b755d3bd05c3acd7b4f6ac
Samuel Liew <samliew@gmail.com>
2021-08-25 11:41:26 +0800

    update guard for mod candidate score

--- src/score.js
+++ src/score.js
@@ -77,7 +77,7 @@ export const makeCandidateScoreCalc = (config, hostname, chatDomain, apiSlug, ap
 
         let responseText = "";
 
-        const findingTargetCandidateScore = isModerator && /what is the candidate score for \d+$/.test(content);
+        const findingTargetCandidateScore = isModerator && /(what is|what's) the candidate score (for|of) \d+$/.test(content);
 
         const wasModerator = modIds.includes(userId);
 
