6b58dd423a060b417902a6fad7303069b7cbbab4
Oleg Valter <o.a.philippov@gmail.com>
2021-10-16 08:28:06 +0300

    slightly improved canned responses builder

--- src/messages.js
+++ src/messages.js
@@ -535,15 +535,12 @@ export const sayAJoke = () => {
  * @summary builds a response to accusations that responses are canned
  * @returns {string}
  */
-export const sayCannedResponses = () => {
-    const responses = new RandomArray(...[
-        "I am a bot, what did you expect?",
-        "Some only deserve canned responses",
-        "Sometimes all you have is a can",
-        "How do I know your message is not canned?"
-    ]);
-    return responses.getRandom();
-};
+export const sayCannedResponses = () => new RandomArray(
+    "I am a bot, what did you expect?",
+    "Some only deserve canned responses",
+    "Sometimes all you have is a can",
+    "How do I know your message is not canned?"
+).getRandom();
 
 /**
  * @summary builds a response to accusations that bot is insane
