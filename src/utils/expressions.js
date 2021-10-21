a4a05eaa4b68a32a2a7aef892ef3072cc6e49a25
Oleg Valter <o.a.philippov@gmail.com>
2021-10-21 06:57:04 +0300

    added allMatch regex helper

--- src/utils/expressions.js
+++ src/utils/expressions.js
@@ -16,3 +16,11 @@ export const matchNumber = (expression, text) => {
  * @returns {boolean}
  */
 export const someMatch = (expressions, text) => expressions.some((expression) => expression.test(text));
+
+/**
+ * @summary AND matches all expressions
+ * @param {RegExp[]} expressions regular expression list
+ * @param {string} text text to test
+ * @returns {boolean}
+ */
+export const allMatch = (expressions, text) => expressions.every((expression) => expression.test(text));
