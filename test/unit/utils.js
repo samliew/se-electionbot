d546098bd47525d3a94a3cfbf105400820cacd76
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-03-16 18:39:28 +0300

    Split integration tests for utilities from unit tests

--- test/unit/utils.js
+++ test/unit/utils.js
@@ -1,9 +1,8 @@
 import { expect } from "chai";
-import { asyncCacheable, fetchChatTranscript, fetchRoomOwners, getSiteDefaultChatroom, listify, numToString, parseBoolEnv, parseIds, parseNumEnv, pluralize, searchChat, stripMarkdown } from "../../src/bot/utils.js";
+import { asyncCacheable, listify, numToString, parseBoolEnv, parseIds, parseNumEnv, pluralize, stripMarkdown } from "../../src/bot/utils.js";
 import { dateToRelativetime } from "../../src/bot/utils/dates.js";
 import { matchNumber } from "../../src/bot/utils/expressions.js";
 import { numericNullable } from "../../src/bot/utils/objects.js";
-import { getMockBotConfig } from "../mocks/bot.js";
 
 describe('RegExp-related utils', () => {
     describe('matchNumber', () => {
@@ -154,74 +153,6 @@ describe('String-related utils', async function () {
         it('should parse id strings correctly', () => {
             const parsed = parseIds("1234|56789|101010");
             expect(parsed).to.deep.equal([1234, 56789, 101010]);
-        });
-    });
-
-    describe('getSiteDefaultChatroom', async () => {
-
-        it('should fetch a site\'s default chat room correctly', async () => {
-            const chatroom = await getSiteDefaultChatroom(getMockBotConfig(), "https://stackoverflow.com");
-
-            expect(chatroom?.chatRoomId).to.equal(197438);
-            expect(chatroom?.chatDomain).to.equal("stackoverflow.com");
-
-            const chatroom2 = await getSiteDefaultChatroom(getMockBotConfig(), "https://superuser.com");
-
-            expect(chatroom2?.chatRoomId).to.equal(118);
-            expect(chatroom2?.chatDomain).to.equal("stackexchange.com");
-        });
-    });
-
-    this.timeout(10e3); // fetching transcript can be slow
-
-    describe('fetchChatTranscript', async () => {
-
-        it('should fetch chat transcript correctly', async () => {
-            const transcriptUrl = "https://chat.stackoverflow.com/transcript/190503/2019/3/20";
-            const chatMessages = await fetchChatTranscript(getMockBotConfig(), transcriptUrl);
-
-            expect(chatMessages).to.not.be.empty;
-
-            expect(chatMessages[0].username).to.equal("Samuel Liew");
-            expect(chatMessages[0].chatUserId).to.equal(584192);
-            expect(chatMessages[0].message).to.equal("how do I vote?");
-            expect(chatMessages[0].date).to.equal(1553052480000);
-            expect(chatMessages[0].messageId).to.equal(45689273);
-
-            expect(chatMessages[1].date).to.equal(1553052481000);
-            expect(chatMessages[2].date).to.equal(1553052482000);
-            expect(chatMessages[3].date).to.equal(1553052483000);
-            expect(chatMessages[4].date).to.equal(1553052484000);
-            expect(chatMessages[5].date).to.equal(1553052485000);
-
-            expect(chatMessages[6].date).to.equal(1553053320000);
-            expect(chatMessages[6].message).to.equal("how do I vote?  This is a test link");
-            expect(chatMessages[6].messageMarkup).to.equal("**how do *I* vote?**  [This is a test link](https://stackoverflow.com/election)");
-        });
-    });
-
-    describe('searchChat', async () => {
-
-        it('should be able to search chat correctly', async () => {
-            const chatMessages = await searchChat(getMockBotConfig(), "stackoverflow.com", "We have a new nomination Please welcome our latest candidate", 238039);
-
-            expect(chatMessages).to.not.be.empty;
-
-            const reversedMessages = chatMessages.reverse();
-
-            expect(reversedMessages[0].username).to.equal("ElectionBot");
-            expect(reversedMessages[0].chatUserId).to.equal(10555677);
-            expect(reversedMessages[0].message).to.equal("We have a new nomination! Please welcome our latest candidate Tamás Sengel!");
-            expect(reversedMessages[0].date).to.equal(1633983300000);
-            expect(reversedMessages[0].messageId).to.equal(53211461);
-        });
-    });
-
-    describe('fetchRoomOwners', async () => {
-
-        it('should fetch chat room owners correctly', async () => {
-            const owners = await fetchRoomOwners(getMockBotConfig());
-            expect(owners.some(user => user.userId === 584192)).to.be.true;
         });
     });
 
