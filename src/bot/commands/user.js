5d15df7d44bc7658256813451e9df105990217e8
Oleg Valter <o.a.philippov@gmail.com>
2022-03-23 01:25:03 +0300

    minor JSDoc improvement for User class convenience getters

--- src/bot/commands/user.js
+++ src/bot/commands/user.js
@@ -8,23 +8,32 @@ export class User {
 
     /**
      * @param {IProfileData} profile
-     * @param {number} access 
+     * @param {number} access
      */
     constructor(profile, access = AccessLevel.user) {
         this.profile = profile;
         this.access = access;
     }
 
+    /**
+     * @summary convenience getter for {@link IProfileData.id}
+     */
     get id() {
         const { profile } = this;
         return profile.id;
     }
 
+    /**
+     * @summary convenience getter for {@link IProfileData.name}
+     */
     get name() {
         const { profile } = this;
         return profile.name;
     }
 
+    /**
+     * @summary convenience getter for {@link IProfileData.reputation}
+     */
     get reputation() {
         const { profile } = this;
         return profile.reputation;
