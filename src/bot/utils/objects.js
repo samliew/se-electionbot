ed5503554ed132cff4ba864424bcbccdc772e0b5
Oleg Valter <o.a.philippov@gmail.com>
2022-04-02 06:42:30 +0300

    added utility for resolving all promises in objects

--- src/bot/utils/objects.js
+++ src/bot/utils/objects.js
@@ -1,5 +1,12 @@
 /**
  * @template {object} T
+ * @typedef {{
+ *  [P in keyof T]: T[P] extends Promise<infer U> ? U : T[P]
+ * }} ResolveObj<T>
+ */
+
+/**
+ * @template {object} T
  *
  * @summary attempts to parse an object property as number
  * @param {T | null | undefined} obj source object
@@ -10,4 +17,22 @@ export const numericNullable = (obj, prop) => {
     if (!obj) return null;
     const value = obj[prop];
     return value !== void 0 && +value || null;
+};
+
+/**
+ * @template {Record<string, any>} T
+ * @param {T} obj object to resolve
+ * @returns {Promise<ResolveObj<T>>}
+ */
+export const resolveObj = async (obj) => {
+    const output = /** @type {ResolveObj<T>} */ ({});
+
+    const promises = Object.entries(obj).map(async ([k, v]) => [k, await v]);
+
+    const resolved = await Promise.all(promises);
+    resolved.forEach(([k, v]) => {
+        output[/** @type {keyof ResolveObj<T>} */(k)] = v;
+    });
+
+    return output;
 };
