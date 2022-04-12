fc9c9df5eefc98f8231fef8b7e7c96f71aca449a
Oleg Valter <o.a.philippov@gmail.com>
2022-04-12 08:52:27 +0300

    added utility for getting routes of an Express app

--- src/server/utils.js
+++ src/server/utils.js
@@ -1,6 +1,9 @@
 /**
  * @typedef {import("express").Application} ExpressApp
  * @typedef {import("http").Server} HttpServer
+ * @typedef {import("express").IRoute} IRoute
+ * @typedef {import("express").IRouter} IRouter
+ * @typedef {Record<string, { methods: string[] }>} RouteInfo
  */
 
 /**
@@ -37,4 +40,31 @@ export const stop = async (server, info) => {
         console.log(error);
         return false;
     }
+};
+
+/**
+ * @summary gets all mounted routes for an {@link ExpressApp}
+ * @param {ExpressApp} app Express application
+ * @returns {RouteInfo}
+ */
+export const routes = (app) => {
+    /** @type {RouteInfo} */
+    const routes = {};
+
+    const { stack } = /** @type {IRouter} */(app._router);
+
+    stack.forEach((item) => {
+        const { route } = /** @type {{route:IRoute}} */(item);
+
+        if (!route) return;
+
+        const { path, stack } = route;
+
+        const methods = new Set();
+        stack.forEach(({ method }) => methods.add(method.toUpperCase()));
+
+        routes[path] = { methods: [...methods] };
+    });
+
+    return routes;
 };
