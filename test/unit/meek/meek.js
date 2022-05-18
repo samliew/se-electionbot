c92574e3ee4074a632e222ba62812fe85f878988
Oleg Valter <o.a.philippov@gmail.com>
2022-05-18 19:50:11 +0300

    ensure dynamic tests run in single-file mode

--- test/unit/meek/meek.js
+++ test/unit/meek/meek.js
@@ -2,10 +2,10 @@ import { expect } from "chai";
 import { readdir, readFile } from "fs/promises";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-
-import MeekSTV from "../../../src/shared/meek/meekstv.js";
 import BallotParser from "../../../src/shared/meek/ballotparser.js";
+import MeekSTV from "../../../src/shared/meek/meekstv.js";
 import TextReport from "../../../src/shared/meek/textreport.js";
+
 
 // Downloading blt files and then parsing is not possible,
 // since ballots are bound to change
@@ -14,25 +14,31 @@ import TextReport from "../../../src/shared/meek/textreport.js";
 //   (site-id.txt => info from site "site", election id "id")
 // Each file contains the election's BLT file, 2 newlines, and the expected output
 
-describe("Ballot counting", async () => {
+describe("Ballot counting", () => {
     const __dirname = dirname(fileURLToPath(import.meta.url));
     const samplePath = join(__dirname, "sample");
 
-    const content = await readdir(samplePath, { encoding: "utf-8" });
+    before(async () => {
+        const fileNames = await readdir(samplePath, { encoding: "utf-8" });
 
-    content.forEach(async fileName => {
-        const filePath = join(__dirname, "sample", fileName);
-        const content = await readFile(filePath);
-        const [expectedOutput, bltContent] = content.toString().split("\n\n\n");
+        describe('should correctly count ballots', () => {
+            for (const fileName of fileNames) {
+                it(`should correctly count ballots in ${fileName}`, async () => {
+                    const filePath = join(__dirname, "sample", fileName);
+                    const content = await readFile(filePath);
+                    const [expectedOutput, bltContent] = content.toString().split("\n\n\n");
 
-        const parsed = new BallotParser().parse(bltContent);
-        const electionInfo = new MeekSTV(parsed);
-        electionInfo.countBallots();
+                    const parsed = new BallotParser().parse(bltContent);
+                    const electionInfo = new MeekSTV(parsed);
+                    electionInfo.countBallots();
 
-        const report = new TextReport(electionInfo).generate();
+                    const report = new TextReport(electionInfo).generate();
 
-        it(`should correctly count ballots in ${fileName}`, () => {
-            expect(report).to.equal(expectedOutput);
+                    expect(report).to.equal(expectedOutput);
+                });
+            }
         });
     });
+
+    it("dummy test for dynamic tests to work", () => { });
 });
