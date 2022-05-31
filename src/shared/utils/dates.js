33bcd6b2e255e793120f228b667ac999dd73ebec
Oleg Valter <o.a.philippov@gmail.com>
2022-05-31 21:16:55 +0300

    added date unit constants

--- src/shared/utils/dates.js
+++ src/shared/utils/dates.js
@@ -1,8 +1,10 @@
 import { pluralize } from "../../bot/utils.js";
 
-const MS_IN_SECOND = 1000;
-
+export const MS_IN_SECOND = 1000;
 export const SEC_IN_MINUTE = 60;
+export const MIN_IN_HOUR = 60;
+export const HOUR_IN_DAY = 24;
+export const MS_IN_DAY = MS_IN_SECOND * SEC_IN_MINUTE * MIN_IN_HOUR * HOUR_IN_DAY;
 
 /**
  * @summary validates and normalizes the Date
