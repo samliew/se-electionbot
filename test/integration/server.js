91449d86075b6c3990cf1e8981c2c8f87f0e32e2
Oleg Valter <o.a.philippov@gmail.com>
2022-06-03 18:17:56 +0300

    added integration test for being able to load the home dashboard route (closes #185)

---
+++ test/integration/server.js
@@ -0,0 +1,63 @@
+import { expect } from "chai";
+import Client from "chatexchange";
+import User from "chatexchange/dist/User.js";
+import { JSDOM } from "jsdom";
+import sinon from "sinon";
+import ScheduledAnnouncement from "../../src/bot/announcement.js";
+import Election from "../../src/bot/election.js";
+import Rescraper from "../../src/bot/rescraper.js";
+import { fetchUrl } from "../../src/bot/utils.js";
+import { startServer } from "../../src/server/index.js";
+import { getHostnamesFromIP } from "../../src/shared/utils/server.js";
+import { getMockBotConfig } from "../mocks/bot.js";
+
+/**
+ * @typedef {import("express").Application} ExpressApp
+ * @typedef {import("http").Server} HttpServer
+ */
+
+describe("Dashboard", () => {
+    /** @type {Client} */
+    const client = new Client["default"]("stackoverflow.com");
+    const room = client.getRoom(92073);
+
+    const config = getMockBotConfig({ debug: false });
+
+    const election = new Election("https://stackoverflow.com/election/13");
+
+    const scraper = new Rescraper(config, client, room, new Map([[13, election]]), election);
+    const announcement = new ScheduledAnnouncement(config, room, election, scraper);
+
+    /** @type {ExpressApp} */
+    let app;
+    before(async () => {
+        await election.scrapeElection(config);
+        app = await startServer(client, room, config, election, announcement);
+    });
+
+    after(() => process.kill(process.pid, "SIGINT"));
+
+    afterEach(() => sinon.restore());
+
+    describe("Routes", () => {
+        it('should correctly render the home route', async () => {
+            const getMeStub = sinon.stub(client, "getMe");
+            getMeStub.resolves(new User["default"](client, 42));
+
+            /** @type {HttpServer} */
+            const server = app.get("server");
+
+            const { address, port } = /** @type {import("net").AddressInfo} */(server.address());
+
+            const [hostname] = await getHostnamesFromIP(address);
+
+            const url = new URL(`http://${hostname}:${port}`);
+
+            const html = await fetchUrl(config, url);
+
+            const { window: { document } } = new JSDOM(html);
+
+            expect(document.querySelector("a[href*='/rooms/190503']")).to.not.be.null;
+        });
+    });
+});
