ebc2f0a0d70df93912badcf989a48139f20c4c71
Oleg Valter <o.a.philippov@gmail.com>
2022-07-14 01:38:57 +0300

    moved election announcements utils to a dedicated module

--- src/bot/election.js
+++ src/bot/election.js
@@ -9,13 +9,16 @@ import { filterMap, getOrInit, has, mergeMaps, sortMap } from '../shared/utils/m
 import { clone } from '../shared/utils/objects.js';
 import { scrapeModerators } from '../shared/utils/scraping.js';
 import { formatOrdinal } from "../shared/utils/strings.js";
-import { getBadges, getMetaSite, getNamedBadges, getNumberOfVoters, getUserInfo, searchQuestions } from './api.js';
+import { getBadges, getNamedBadges, getNumberOfVoters, getUserInfo } from './api.js';
+import { getElectionAnnouncements } from "./elections/announcements.js";
 import History from "./history.js";
 import { calculateScore } from './score.js';
 import { fetchUrl, onlyBotMessages, scrapeChatUserParentUserInfo, searchChat } from './utils.js';
 
 /**
+ * @typedef {import("./elections/announcements.js").ElectionAnnouncement} ElectionAnnouncement
  * @typedef {null|"ended"|"election"|"primary"|"nomination"|"cancelled"} ElectionPhase
+ * @typedef {import("./elections/announcements.js").ElectionType} ElectionType
  * @typedef {"electionWithPrimary"|"electionWithoutPrimary"|"nomination"|"primary"|"announcement"} ElectionPhaseDuration
  * @typedef {import("./index").ElectionBadge} ElectionBadge
  * @typedef {import('chatexchange/dist/Client').Host} Host
@@ -32,19 +35,6 @@ import { fetchUrl, onlyBotMessages, scrapeChatUserParentUserInfo, searchChat } f
  *  election?: number,
  *  electionLink?: string
  * }} ModeratorUser
- *
- * @typedef {"full" | "graduation" | "pro-tempore"} ElectionType
- *
- * @typedef {{
- *  dateAnnounced: string,
- *  dateNomination: string,
- *  postLink: string,
- *  postTitle: string,
- *  type: ElectionType,
- *  userId: number,
- *  userLink: string,
- *  userName: string
- * }} ElectionAnnouncement
  */
 
 /**
@@ -343,86 +333,6 @@ export const scrapeQuestionnaire = ($, el) => {
     const questions = [];
     $(el).find("blockquote ol li").each((_, e) => questions.push($(e).text()));
     return questions;
-};
-
-/** @type {Map<string, Map<number, ElectionAnnouncement>>} */
-const announcementsCache = new Map();
-
-/**
- * @summary gets upcoming election announcements
- * @param {BotConfig} config bot configuration
- * @param {Election} election current election
- * @returns {Promise<Map<string, Map<number, ElectionAnnouncement>>>}
- */
-export const getElectionAnnouncements = async (config, election) => {
-    // https://regex101.com/r/OTlwms/3
-    const announcementTitleExpr = /^announc(?:ing|ement).+?election.*?|initial(?:\s+pro(?:\s+|-)tem(?:pore)?)\s+moderator\s+election/i;
-
-    // https://regex101.com/r/GKcR6r/2
-    const proTemporeTitleExpr = /\bpro(?:\s+|-)tem(?:pore)?\b/i;
-
-    // https://regex101.com/r/Qb8pB1/1
-    const graduationExpr = /\bgraduation|(?:first|1st)\s+full\b/i;
-
-    // https://regex101.com/r/uXY3xB/4
-    const electionDateExpr = /(?<!beta\s+)on\s+(?:<(?:strong|em|i)>)?(\d{1,2}\s+\w+|\w+\s+\d{1,2})(?:,?\s+(\d{2,4}))?/i;
-
-    const { apiSlug, siteHostname } = election;
-
-    const metaSite = await getMetaSite(config, apiSlug);
-    if (!metaSite) {
-        console.log(`[announcements] missing meta site for ${apiSlug}`);
-        return announcementsCache;
-    }
-
-    const announcements = getOrInit(announcementsCache, siteHostname, new Map());
-
-    const posts = await searchQuestions(config, metaSite.api_site_parameter, {
-        intitle: "election",
-    });
-
-    const time = config.get("default_election_time", "20:00:00");
-
-    posts.forEach((post) => {
-        const { body, creation_date, owner, question_id, link, title } = post;
-        if (!body || !owner || !title) {
-            console.log(`[announcements] missing post info body (${apiSlug})`);
-            return;
-        }
-
-        if (!announcementTitleExpr.test(title)) return;
-
-        const { user_id: userId, link: userLink, display_name: userName } = owner;
-        if (!userId || !userLink || !userName) {
-            console.log(`[announcements] missing post owner fields (${apiSlug})`);
-            return;
-        }
-
-        const postedAt = new Date(creation_date * 1000);
-
-        const [_, monthday, year = postedAt.getFullYear()] = electionDateExpr.exec(body) || [];
-
-        const dateElection = dateToUtcTimestamp(`${monthday}, ${year} ${time}Z`);
-        const dateAnnounced = dateToUtcTimestamp(postedAt);
-
-        /** @type {ElectionAnnouncement} */
-        const upcomingElectionAnnouncement = {
-            dateAnnounced,
-            dateNomination: dateElection,
-            postLink: link,
-            postTitle: title,
-            type: proTemporeTitleExpr.test(title) ?
-                "pro-tempore" :
-                graduationExpr.test(title) || graduationExpr.test(body) ? "graduation" : "full",
-            userId,
-            userLink,
-            userName
-        };
-
-        announcements.set(question_id, upcomingElectionAnnouncement);
-    });
-
-    return announcementsCache;
 };
 
 export class Nominee {
---
+++ src/bot/elections/announcements.js
@@ -0,0 +1,101 @@
+import { dateToUtcTimestamp } from "../../shared/utils/dates.js";
+import { getOrInit } from "../../shared/utils/maps.js";
+import { getMetaSite, searchQuestions } from "../api.js";
+
+/**
+ * @typedef {import("../config.js").BotConfig} BotConfig
+ * @typedef {import("../election.js").default} Election
+ *
+ * @typedef {"full" | "graduation" | "pro-tempore"} ElectionType
+ *
+ * @typedef {{
+ *  dateAnnounced: string,
+ *  dateNomination: string,
+ *  postLink: string,
+ *  postTitle: string,
+ *  type: ElectionType,
+ *  userId: number,
+ *  userLink: string,
+ *  userName: string
+ * }} ElectionAnnouncement
+ */
+
+/** @type {Map<string, Map<number, ElectionAnnouncement>>} */
+const announcementsCache = new Map();
+
+/**
+ * @summary gets upcoming election announcements
+ * @param {BotConfig} config bot configuration
+ * @param {Election} election current election
+ * @returns {Promise<Map<string, Map<number, ElectionAnnouncement>>>}
+ */
+export const getElectionAnnouncements = async (config, election) => {
+    // https://regex101.com/r/OTlwms/3
+    const announcementTitleExpr = /^announc(?:ing|ement).+?election.*?|initial(?:\s+pro(?:\s+|-)tem(?:pore)?)\s+moderator\s+election/i;
+
+    // https://regex101.com/r/GKcR6r/2
+    const proTemporeTitleExpr = /\bpro(?:\s+|-)tem(?:pore)?\b/i;
+
+    // https://regex101.com/r/Qb8pB1/1
+    const graduationExpr = /\bgraduation|(?:first|1st)\s+full\b/i;
+
+    // https://regex101.com/r/uXY3xB/4
+    const electionDateExpr = /(?<!beta\s+)on\s+(?:<(?:strong|em|i)>)?(\d{1,2}\s+\w+|\w+\s+\d{1,2})(?:,?\s+(\d{2,4}))?/i;
+
+    const { apiSlug, siteHostname } = election;
+
+    const metaSite = await getMetaSite(config, apiSlug);
+    if (!metaSite) {
+        console.log(`[announcements] missing meta site for ${apiSlug}`);
+        return announcementsCache;
+    }
+
+    const announcements = getOrInit(announcementsCache, siteHostname, new Map());
+
+    const posts = await searchQuestions(config, metaSite.api_site_parameter, {
+        intitle: "election",
+    });
+
+    const time = config.get("default_election_time", "20:00:00");
+
+    posts.forEach((post) => {
+        const { body, creation_date, owner, question_id, link, title } = post;
+        if (!body || !owner || !title) {
+            console.log(`[announcements] missing post info body (${apiSlug})`);
+            return;
+        }
+
+        if (!announcementTitleExpr.test(title)) return;
+
+        const { user_id: userId, link: userLink, display_name: userName } = owner;
+        if (!userId || !userLink || !userName) {
+            console.log(`[announcements] missing post owner fields (${apiSlug})`);
+            return;
+        }
+
+        const postedAt = new Date(creation_date * 1000);
+
+        const [_, monthday, year = postedAt.getFullYear()] = electionDateExpr.exec(body) || [];
+
+        const dateElection = dateToUtcTimestamp(`${monthday}, ${year} ${time}Z`);
+        const dateAnnounced = dateToUtcTimestamp(postedAt);
+
+        /** @type {ElectionAnnouncement} */
+        const upcomingElectionAnnouncement = {
+            dateAnnounced,
+            dateNomination: dateElection,
+            postLink: link,
+            postTitle: title,
+            type: proTemporeTitleExpr.test(title) ?
+                "pro-tempore" :
+                graduationExpr.test(title) || graduationExpr.test(body) ? "graduation" : "full",
+            userId,
+            userLink,
+            userName
+        };
+
+        announcements.set(question_id, upcomingElectionAnnouncement);
+    });
+
+    return announcementsCache;
+};
